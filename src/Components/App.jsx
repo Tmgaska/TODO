90c621211e28fd7220a3e7edc6671e0d97202ed4
Your User Name <tmgaska@gmail.com>
2025-10-21 19:23:08 +0900

    edit

--- src/Components/App.jsx
+++
@@ -1,6 +0,0 @@
-import TodoList from'./TodoList.jsx';
-import App from './App.tsx';
-function App(){
-  return(<TodoList/>)
-}
-export default App;
--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,4 +1,4 @@
-import TodoList from './TodoList.jsx'
+import TodoList from './TodoList.tsx'
 function App() {
   return <TodoList />
 }
