c3e72764fff2e5ac71d346bdc60788320e6c851d
Your User Name <tmgaska@gmail.com>
2025-12-09 20:35:06 +0900

    Remove build artifacts from Git tracking

--- backend/Models/TodoItemDTO.cs
+++ backend/Models/TodoItemDTO.cs
@@ -2,24 +2,11 @@
 
 public class TodoItemDTO
 {
-    public long Id { get; set; }
+    public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 
     public string? DueDate { get; set; }
     public string? CompletedDate { get; set; }
 
-}
-public class TodoItemCreateDTO
-{
-    public string Name { get; set; } = null!;
-    public bool IsComplete { get; set; } = false;
-    public DateTime? DueDate { get; set; }
-}
-
-public class TodoItemUpdateDTO
-{
-    public string Name { get; set; } = null!;
-    public bool IsComplete { get; set; }
-    public DateTime? DueDate { get; set; }
 }
